8a7e873b9c7e37e46b55e16e1cfd185ab3b4c117
kfallonwork <katie.fallon@waterstons.com>
2023-09-06 14:50:33 +0100

    made it look pretty

--- first_bot.py
+++ first_bot.py
@@ -11,11 +11,24 @@ st.set_page_config(
     page_icon=logo,
 )
 
+with st.sidebar:
+    image_path = Path('streamlit/images/logo1.png')
+    logo = Image.open(image_path)
+    st.image(logo)
+    st.title("Welcome to Waterstons Innovation bot!")
+    st.markdown("This chatbot will answer all of your questions about robot dogs.")
+    st.markdown("For more information about our team and what we get up to please check out our **substack:** https://waterstonsinnovation.substack.com/")
 
-image_path = Path('streamlit/images/banner1.png')
+
+image_path = Path('streamlit/images/robot_dog.png')
 image1 = Image.open(image_path)
-st.image(image1)
-st.markdown("Welcome to the Waterstons Innovation bot! This chatbot will answer all of your queries relating to robot dogs.")
+st.image(image1, use_column_width=True)
+
+
+image_path = Path('streamlit/images/assistant.png')
+assistant_img = Image.open(image_path)
+image_path = Path('streamlit/images/user.png')
+user_img = Image.open(image_path)
 
 st.title("") 
 if "messages" not in st.session_state:
@@ -25,16 +38,13 @@ if "messages" not in st.session_state:
         {"role": "user", "content": "What is the capital of England?"},
         {"role": "assistant", "content": "I only answer questions about robot dogs"},
     ]
-
-#for msg in st.session_state.messages:
-st.chat_message("assistant").write("Can I interest you in a robot dog?")
-
+st.chat_message("assistant", avatar=assistant_img).write("Can I interest you in a robot dog?")
 if prompt := st.chat_input():
 
     openai.api_key = st.secrets.api_credentials.api_key
     st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
+    st.chat_message("user", avatar=user_img).write(prompt)
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
     msg = response.choices[0].message
     st.session_state.messages.append(msg)
-    st.chat_message("assistant").write(msg.content)
+    st.chat_message("assistant", avatar=assistant_img).write(msg.content)
