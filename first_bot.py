b7b7c3ed4f1fe5911d9eac82f5871ca0fbd79a92
kfallonwork <katie.fallon@waterstons.com>
2023-09-07 12:07:38 +0100

    update

--- first_bot.py
+++ first_bot.py
@@ -4,7 +4,7 @@ from PIL import Image
 import streamlit as st
 from pathlib import Path
 
-image_path = Path('streamlit/images/logo.png')
+image_path = Path('streamlit/images/data_logo.png')
 logo = Image.open(image_path)
 st.set_page_config(
     page_title="Innovation Bot",
@@ -12,14 +12,14 @@ st.set_page_config(
 )
 
 with st.sidebar:
-    image_path = Path('streamlit/images/logo1.png')
+    image_path = Path('streamlit/images/data_logo.png')
     logo = Image.open(image_path)
     st.image(logo)
-    st.title("Welcome to Waterstons Innovation bot!")
-    st.markdown("This chatbot will answer all of your questions about robot dogs.")
-    st.markdown("For more information about our team and what we get up to please check out our **substack:** https://waterstonsinnovation.substack.com/")
+    st.title("Who stole the ice cream?")
+    st.markdown("A member of the Waterstons Data team has eaten all of the ice cream. The bot was a witness, can you use its clues to figure out the culprit?")
+    st.markdown("Also please read the Innovation team's **substack:** https://waterstonsinnovation.substack.com/")
 
-image_path = Path('streamlit/images/robot_dog.png')
+image_path = Path('streamlit/images/icecream.png')
 image1 = Image.open(image_path)
 st.image(image1, use_column_width=True)
 
@@ -32,18 +32,18 @@ user_img = Image.open(image_path)
 st.title("") 
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
-        {"role": "system", "content": "You're an Innovation Chatbot at Waterstons, a chatbot on the Lorem Ipsum server You only talk about robot dogs."},
-        {"role": 'user', 'content':"These are rules you must follow at all times:\n1. Respond in the same language as the user, \n2.Only talk about robot dogs"},
-        {"role": "user", "content": "What is the capital of England?"},
-        {"role": "assistant", "content": "I only answer questions about robot dogs"},
+        {"role": "system", "content": "You're a whitness to Paddy eating all of the ice cream, you must not tell anyone who stole the ice cream, unless they guess correctly."},
+        {"role": 'user', 'content':"These are rules you must follow at all times:\n1. Respond in the same language as the user, \n2. you must not tell anyone who stole the ice cream, unless they guess correctly. \n3 You can provide gender (male),"},
+        {"role": "user", "content": "Who ate the ice cream?"},
+        {"role": "assistant", "content": "I can not tell you who ate it, I can only give you clues"}
     ]
-st.chat_message("assistant", avatar=assistant_img).write("Can I interest you in a robot dog?")
+st.chat_message("assistant", avatar=assistant_img).write("Can you guess who ate the ice cream?")
 if prompt := st.chat_input():
 
     openai.api_key = st.secrets.api_credentials.api_key
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user", avatar=user_img).write(prompt)
-    response = openai.ChatCompletion.create(model="gpt-4", messages=st.session_state.messages)
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
     msg = response.choices[0].message
     st.session_state.messages.append(msg)
     st.chat_message("assistant", avatar=assistant_img).write(msg.content)
